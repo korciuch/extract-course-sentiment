5443a96f37097a4ed158714719a4e3cdd8e95731
Kyle Orciuch <korciuch@stanford.edu>
2021-12-12 01:23:24 -0800

    final fall commit

--- src/algorithms/pkgs.py
+++ src/algorithms/pkgs.py
@@ -1,11 +1,15 @@
 import os
 import csv
+import nltk
 import nltk.data
 import regex as re
 import networkx as nx
+
 from pymongo import MongoClient
 from nltk.corpus import stopwords
+from collections import defaultdict
 from nltk.stem import WordNetLemmatizer
 from nltk.parse.corenlp import CoreNLPServer
+from nltk.sentiment import vader
 from nltk.parse import CoreNLPParser, CoreNLPDependencyParser
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, _preprocess
--- src/algorithms/statics.py
+++ src/algorithms/statics.py
@@ -1,5 +1,5 @@
 N_GRAM_LOWER_LIM = 1
-N_GRAM_UPPER_LIM = 2
+N_GRAM_UPPER_LIM = 1
 SERVER_URL = 'http://localhost:9000'
 DB_CONNECTION = 'mongodb://localhost:27017'
 HEADER_ROW =    [
@@ -33,13 +33,18 @@ HEADER_ROW =    [
                     "ANSWER_SET_TEXT",
                     "ANSWER_SET_NUM"
                 ]
-                
 RELEVANT_ROWS = [
                     "COURSE_UNIQUE_ID",
+                    "QUESTION_ID",
+                    "RESPONSE_UNIQUE_ID",
                     "COURSE_TITLE",
                     "INSTRUCTOR_NAME",
-                    "QUESTION_ID",
-                    "QUESTION_TEXT",
                     "RESPONSE_TEXT"
                 ]
-                
+CUSTOM_STOPWORDS = ['dr','would','also','us','couldn','ve','nt',
+                    'great','lot','interesting','much','quite',
+                    'take', 'learned', 'better', 'really', 'even', 'th',
+                    'like', 'learn', 'learning', 'enough', 'important', 
+                    'pretty', 'better', 'good', 'bad', 'worse', 'worst', 
+                    'awesome', 'fantastic', 'horrible', 'phenomenal', 
+                    'horrid', 'unbelievable', 'best', 'much', 'lot', 'done']
